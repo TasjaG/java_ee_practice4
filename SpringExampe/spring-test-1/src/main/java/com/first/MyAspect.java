04e72655e5404d17b5eef857af4435b491a8bd6b
TasjaG <tasjagrigorieva@gmail.com>
2017-03-01 01:58:38 +0200

    Fixed dependencies

--- SpringExampe/spring-test-1/src/main/java/com/first/MyAspect.java
+++ SpringExampe/spring-test-1/src/main/java/com/first/MyAspect.java
@@ -1,7 +1,32 @@
 package com.first;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class MyAspect {
-
+	@Before("execution(* HelloBean.setName(..))")
+    public void logBeforeV1(JoinPoint joinPoint) 
+    {
+        System.out.println("MyAspect.logBeforeV1() : " + joinPoint.getSignature().getName());
+    }
+     
+    @Before("execution(* HelloBean.*(..))")
+    public void logBeforeV2(JoinPoint joinPoint) 
+    {
+        System.out.println("MyAspect.logBeforeV2() : " + joinPoint.getSignature().getName());
+    }
+     
+    @After("execution(* HelloBean.setName(..))")
+    public void logAfterV1(JoinPoint joinPoint) 
+    {
+        System.out.println("MyAspect.logAfterV1() : " + joinPoint.getSignature().getName());
+    }
+     
+    @After("execution(* HelloBean.*(..))")
+    public void logAfterV2(JoinPoint joinPoint) 
+    {
+        System.out.println("MyAspect.logAfterV2() : " + joinPoint.getSignature().getName());
+    }
 }
